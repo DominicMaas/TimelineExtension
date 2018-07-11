9bfb0065a8948c67b90b1db70becc41438b6f0f5
Dominic Maas <dominic.maas@live.com>
2018-07-11 19:03:07 +1200

    Correctly get the access token

--- popup.js
+++ popup.js
@@ -16,8 +16,8 @@ function Login() {
     // Build the request url
     let authURL = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize';
         authURL += `?client_id=${ClientID}`;
-        authURL += `&response_type=code`;
-        authURL += `&response_mode=query`;
+        authURL += `&response_type=token`;
+        authURL += `&response_mode=fragment`;
         authURL += `&redirect_uri=${encodeURIComponent('https://mmpefenajbmfcmgfadibmkmancoljggp.chromiumapp.org/provider_cb')}`;
         authURL += `&scope=${encodeURIComponent(Scopes.join(' '))}`;
 
@@ -26,7 +26,23 @@ function Login() {
         'url': authURL,
         'interactive': true
     }, redirect_url => {
-        let code = (redirect_url.match(/code=([^&]+)/))[1];
-        alert(code);
+        // Get the data from the redirect url
+        let data = redirect_url.split('#')[1];
+
+        // Split the data into pairs
+        var pairsArray = data.split('&');
+        
+        // Object that will store the key value pairs
+        var pairsKeyValuePair = {};
+
+        // Store the data into a key value pair object
+        for (pair in pairsArray) {
+            var split = pairsArray[pair].split('=');
+            pairsKeyValuePair[decodeURIComponent(split[0])] = decodeURIComponent(split[1]);
+        }
+
+        alert(pairsKeyValuePair["access_token"]);
+
+        console.log(pairsArray);
     });
 }
