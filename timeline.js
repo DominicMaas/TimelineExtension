b496456e5b9e13e73f4aa3127b0c376142da404a
Dominic Maas <dominic.maas@live.com>
2018-07-12 17:22:47 +1200

    Fix location origin is null

--- timeline.js
+++ timeline.js
@@ -2,16 +2,16 @@
 (document.readyState == 'complete' || document.readyState == 'interactive') 
     ? CreateOrUpdateActivity() : document.addEventListener('DOMContentLoaded', CreateOrUpdateActivity);
 
-
 // Create or update the activity
 function CreateOrUpdateActivity() {
-
     // Activity data
     let title = (document.querySelector('meta[property="og:title"],meta[name="og:title"]') === null) ? document.title : document.querySelector('meta[property="og:title"],meta[name="og:title"]').content;
     let url = (document.querySelector('link[rel~="canonical"][href]') === null) ? ((document.querySelector('meta[property="og:url"][content],meta[name="og:url"][content]') === null) ? document.location.toString() : document.querySelector('meta[property="og:url"][content],meta[name="og:url"][content]').content) : document.querySelector('link[rel~="canonical"][href]').href;
-    let origin = new URL(activityDescription).hostname;
+    let origin = location.origin.replace(/(^\w+:|^)\/\//, '');
     let image = (document.querySelector('meta[property="og:image"],meta[name="og:image"]') === null) ? '' : document.querySelector('meta[property="og:image"],meta[name="og:image"]').content;
 
+    // Build the activity message to send to the 
+    // background thread.
     let activityMessage = {
         type: 'WebActivity',
         payload: {
