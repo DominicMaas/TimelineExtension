71b6fc15c2167d8a240287d9ca0640ab648227b8
Daniel Aleksandersen <code@daniel.priv.no>
2018-07-12 18:28:18 +0200

    Settings toggle

--- menu/menu.js
+++ menu/menu.js
@@ -1,5 +1,19 @@
+var settingsToggle = false;
+
 // Run when document has loaded
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Get the login button and bind the click event
+    document.getElementById('toggle-settings').addEventListener('click', function() {
+
+        document.getElementById('section-login').style.display = settingsToggle ? 'block' : 'none';
+        document.getElementById('section-settings').style.display = settingsToggle ? 'none' : 'block';
+
+        document.getElementById('toggle-settings').textContent = settingsToggle ? 'Settings' : 'Back';
+
+        settingsToggle = !settingsToggle;
+    });
+
     // Get the login button and bind the click event
     document.getElementById('login').addEventListener('click', function() {
         // Create the login message
