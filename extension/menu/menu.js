7ca61e657e9bc8599930bb79097042bebcf94faa
Dominic Maas <dominic.maas@live.com>
2018-07-14 23:47:18 +1200

    UI Updates

--- extension/menu/menu.js
+++ extension/menu/menu.js
@@ -52,8 +52,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // https://github.com/danklammer/bytesize-icons
         document.getElementById('toggle-settings').innerHTML = settingsToggle 
-        ? '<svg id="i-settings" viewBox="0 0 32 32" width="24" height="24" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M13 2 L13 6 11 7 8 4 4 8 7 11 6 13 2 13 2 19 6 19 7 21 4 24 8 28 11 25 13 26 13 30 19 30 19 26 21 25 24 28 28 24 25 21 26 19 30 19 30 13 26 13 25 11 28 8 24 4 21 7 19 6 19 2 Z" /><circle cx="16" cy="16" r="4" /></svg>' 
-        : '<svg id="i-home" viewBox="0 0 32 32" width="24" height="24" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M12 20 L12 30 4 30 4 12 16 2 28 12 28 30 20 30 20 20 Z" /></svg>';
+        ? '<svg id="i-settings" viewBox="0 0 32 32" width="16" height="16" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M13 2 L13 6 11 7 8 4 4 8 7 11 6 13 2 13 2 19 6 19 7 21 4 24 8 28 11 25 13 26 13 30 19 30 19 26 21 25 24 28 28 24 25 21 26 19 30 19 30 13 26 13 25 11 28 8 24 4 21 7 19 6 19 2 Z" /><circle cx="16" cy="16" r="4" /></svg>' 
+        : '<svg id="i-home" viewBox="0 0 32 32" width="16" height="16" fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M12 20 L12 30 4 30 4 12 16 2 28 12 28 30 20 30 20 20 Z" /></svg>';
 
         settingsToggle = !settingsToggle;
     });
@@ -84,10 +84,15 @@ document.addEventListener('DOMContentLoaded', function() {
                     // Get the device
                     let device = data.payload[index];
                     
+                    // Platform=Windows/Android/iOS, Model=Surface, Kind=Mobile/Laptop/PC/Tablet
+
                     console.log(device);
 
+                    // Get the description
+                    var description = device.Model == null ? device.Platform + ' Device' : device.Model;
+
                     // Update UI
-                    devicesHolder.insertAdjacentHTML('beforeend', '<a id="remote-device-'+device.id+'" class="remote-device"><p>'+device.Name+'</p></div>');
+                    devicesHolder.insertAdjacentHTML('beforeend', '<a id="remote-device-'+device.id+'" class="inline-button"><div class="status-icon '+device.Status+'"></div><p>'+device.Name+'<p class="description">'+description+'</p></p></a>');
 
                     // Attach click event
                     document.getElementById('remote-device-' + device.id).addEventListener('click', function() {
@@ -95,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                             // Get the active tab
                             var activeTab = tabs[0];
-                            
+                        
                             // Create the navigate message
                             let remoteNavigate = { type: 'RemoteNavigate', payload: { id: device.id, url: activeTab.url } };
                             // Send the navigate request to the background
