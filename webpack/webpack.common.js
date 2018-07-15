1c29f9d6e5e215f6337d426b083619383a3b240e
Dominic Maas <dominic.maas@live.com>
2018-07-15 12:11:44 +1200

    Update webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,28 +4,13 @@ module.exports = {
     entry: {
         background: path.join(__dirname, '../src/scripts/background.ts'),
         menu: path.join(__dirname, '../src/scripts/menu.ts'),
-        timeline: path.join(__dirname, '../sr/scripts/timeline.ts')
+        timeline: path.join(__dirname, '../src/scripts/timeline.ts')
     },
     output: {
         path: path.join(__dirname, '../extension/scripts'),
         filename: '[name]-complied.js'
     },
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            chunks: "initial"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: ['.ts', '.js']
     }
 };
